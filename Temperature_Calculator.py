f026acbac3633eb63ee39b7e881001014e52ec87
Alexa <hello.i.am.aqsa@gmail.com>
2026-02-13 13:03:00 +0500

    Add files via upload

---
+++ Temperature_Calculator.py
@@ -0,0 +1,21 @@
+# Temperature Calculator in Fahrenheit and Celsius
+
+# Step 1: Temprature in Celsius
+celsius = float(input("\nEnter Temperature in Celsius: "))
+
+# Step 2: Convert in Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Step 3: Display temp. in Fahrenheit
+print(f"Temprature in Fahrenheit: {fahrenheit:.2f}°F")
+
+# Temperature Calculator in Fahrenheit and Celsius
+
+# Step 1: Temprature in Fahrenheit
+fahrenheit = float(input("\nEnter Temprature in Fahrenheit: "))
+
+# Step 2: Convert in Celsius 
+celsius = (fahrenheit - 32) * 5/9
+
+# Step 3: Display Temprature in Celsius
+print(f"Temprature in Celsius: {celsius:.2f}°C")
